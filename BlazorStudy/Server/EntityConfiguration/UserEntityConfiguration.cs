93fda93dde5a720485f4b0007ea692d8314172c3
ahmedyounis253 <amy2532001@gmail.com>
2022-09-10 20:03:58 +0200

    Add User

---
+++ BlazorStudy/Server/EntityConfiguration/UserEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorStudy.Server;
+
+public class UserEntityConfiguration:BaseEntityConfiguration<User>,IEntityTypeConfiguration<User>
+{
+    public override void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasIndex(u => u.EmailAddress).IsUnique();
+        
+        builder.Property(x => x.EmailAddress).IsRequired();
+        builder.Property(x => x.Password).IsRequired();
+
+
+        base.Configure(builder);
+    }
+
+    
+}
--- BlazorStudy/Server/GlobalUsing.cs
+++ BlazorStudy/Server/GlobalUsing.cs
@@ -1,4 +1,6 @@
 ﻿global using BlazorStudy.Shared;
 
-global using Microsoft.EntityFrameworkCore;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.EntityFrameworkCore.ValueGeneration;
