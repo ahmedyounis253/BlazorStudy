84ee7d0ffd82ad030c8af9767e82cdc34278bc79
ahmedyounis253 <amy2532001@gmail.com>
2022-09-10 20:04:30 +0200

    Add Message

---
+++ BlazorStudy/Server/EntityConfiguration/MessageEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorStudy.Server.EntityConfiguration
+{
+    public class MessageEntityConfiguration:BaseEntityConfiguration<Message>,IEntityTypeConfiguration<Message>
+    {
+        public override void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.ToTable("Messages");
+
+            builder.Property(x=>x.MessageText).IsRequired();
+            builder.Property(x => x.MessageText).HasMaxLength(2000);
+
+            builder.HasOne(e => e.FromUser).WithMany().HasForeignKey(e => e.FromUserId).OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(e => e.ToUser).WithMany().HasForeignKey(e => e.ToUserId).OnDelete(DeleteBehavior.NoAction);
+
+            base.Configure(builder);
+        }
+    }
+}
