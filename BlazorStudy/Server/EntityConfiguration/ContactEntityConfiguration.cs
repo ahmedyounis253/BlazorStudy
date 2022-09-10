ec7f462048bd4e2f5d2ecd7884d6bcea4994ada7
ahmedyounis253 <amy2532001@gmail.com>
2022-09-10 20:04:42 +0200

    Add Contact

---
+++ BlazorStudy/Server/EntityConfiguration/ContactEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorStudy.Server;
+
+public class ContactEntityConfiguration:BaseEntityConfiguration<Contact>,IEntityTypeConfiguration<Contact>
+{
+    public override void Configure(EntityTypeBuilder<Contact> builder)
+    {
+        builder.ToTable("Contacts");
+
+        builder.HasIndex(x => x.PhoneNumber).IsUnique();
+        builder.Property(x => x.ContactType).IsRequired();
+
+        builder.HasOne(x=>x.User).WithMany().HasForeignKey(x=>x.UserId).OnDelete(DeleteBehavior.Cascade);
+
+        base.Configure(builder);
+    }
+}
---
+++ BlazorStudy/Shared/Entities/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorStudy.Shared;
+
+public class Contact:BaseEntity
+{
+    public ContactType ContactType { get; set; }=ContactType.Mobile;
+        
+    public string? PhoneNumber { get; set;  }
+
+    public Guid UserId { get; set; }
+    
+    public User? User { get; set; }  
+}
